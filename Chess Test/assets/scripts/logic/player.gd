316edee236450b76b619149bd0bc8c5c7a9b933e
Thomas-618 <107697159+Thomas-618@users.noreply.github.com>
2022-09-24 11:49:24 -0500

    Cleaning Github

--- Chess Test/assets/scripts/logic/player.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-class Player extends Node:
-	pass
--- Chess Test/assets/unverified/pieces/queen.gd
+++
@@ -1,6 +0,0 @@
-extends Piece
-#class_name Queen
-
-func _init(node_ = null, type_ = 6, color_ = true, square_ = [0,0]).(node_, type_, color_, square_):
-	
-	direction = [[1,1], [-1,1], [-1,-1], [1,-1], [1,0], [0,1], [-1,0], [0,-1]]
